dcfdb5ce181390a3dc568b064bfa84c002884672
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-19 11:05:02 +0700

    Sep-19-2024: add project from scratch with webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Resolve .js and .jsx extensions
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Use both .js and .jsx
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    // contentBase: './dist',
+    static: {
+        directory: path.join(__dirname, 'public'), // Adjust the path as needed
+      },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development', // Change to 'production' for production build
+};
